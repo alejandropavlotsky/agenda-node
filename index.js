38c24a8d2ad6ac02d44348dc41c4c9e0c76dc09a
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2024-08-19 13:36:23 +0200

    add line to run dist folder

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'))
 
 morgan.token('type', function (req, res) {
   return JSON.stringify(req.body);
